dab9fc99773c622d2073c438bfd4c4d9df45f9a9
senamile <mtshalisenamile@gmail.com>
2023-04-17 14:42:48 +0200

    my first project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using System;
+namespace helloWorld
+{
+    class Program
+    {
+        static void main(string[]args)
+        { Console writeline("hello world!");
+        }
+    }
+}
+Console.WriteLine()
